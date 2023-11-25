ce1c5e5ca870bf776ceb95cab4afa3ff421d5207
Afta2003 <aftabestz@gmail.com>
2023-11-25 16:01:03 +0700

    first commit

---
+++ resources/views/facilities.blade.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title> facilities </title>
+    <style>
+        hr {
+            color:aliceblue;
+            width: 1249px
+            left: 250px
+        }
+            div.a{
+            text-align: center;
+        }
+
+        </style>
+  </head>
+  <body>
+    <img src="img/LogoVilla.png" alt="Logo villa"  width="581" height="133" length="1000">
+    <hr>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-bluenavy">
+      <div class="container-fluid justify-content-between d-flex">
+          <a class="navbar-brand" href="#">Dashboard</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/dashboard">Home</a>              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/facilities">Facilities</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/testimonies">testimonies</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/galery">galery</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/feedback">feedback</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/About">About</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div class="container mt-4">
+        <div class="a">
+      <h1 style="color:white;">Our Facilities<h1> 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
